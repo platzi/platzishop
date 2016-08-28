37d90b46516d13f1a737f7ea4c1ab3bffc31d487
slifszyc <sacha.lifszyc@gmail.com>
2016-08-28 18:42:02 -0500

    Formularios con Feedback en el Footer

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const $form = $('#suscribeForm')
+const $formGroup = $form.find('.form-group')
+const $formControl = $formGroup.find('.form-control')
+
+$form.submit((ev) => {
+  ev.preventDefault()
+  
+  const error = Math.random() > 0.5
+  $formGroup.removeClass().addClass('form-group')
+  $formControl.removeClass().addClass('form-control')
+  $formGroup.addClass(error ? 'has-danger' : 'has-success')
+  $formControl.addClass(error ? 'form-control-danger' : 'form-control-success')
+  $formGroup.find('.form-control-feedback').remove()
+  var el;
+  if (error) {
+    el = $('<div>', { html: 'Ha ocurrido un error' })
+  } else {
+    el = $('<div>', { html: 'Te enviaremos todas las novedades!' })
+  }
+  el.addClass('form-control-feedback')
+  $formGroup.append(el)
+})
